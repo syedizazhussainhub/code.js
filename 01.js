2a687224b0c5f773caf53ceb3d17160f39223e6b
Syed Izaz Hussain <syedizazhussainemail@gmail.com>
2024-06-11 13:04:20 +0500

    commit config

--- 01.js
+++ 01.js
@@ -1,1 +1,13 @@
-console.log("izaz");
+//Let Const Var
+
+
+
+const acc_id = 6463646;
+let acc_email = "syedizazhussainhun";
+var acc_password = "637473";
+accoubtcity= "isl"
+
+
+
+// acc_id=5253;
+console.log(acc_id);
